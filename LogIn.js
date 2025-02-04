441c90bb12fb8559d09611f027ea8aa8f53da8f2
Max Kleeberg <103602295+SternGang@users.noreply.github.com>
2025-02-04 09:55:32 -0500

    Add files via upload

---
+++ LogIn.js
@@ -0,0 +1,14 @@
+let logInButton = document.getElementById("LogInButton");
+
+function checklog(){
+    let username = document.getElementById("UserName").value;
+    let PassWord = document.getElementById("PassWord").value;
+
+    console.log(username);
+    console.log(PassWord);
+}
+
+logInButton.addEventListener("click",checklog);
+
+
+
