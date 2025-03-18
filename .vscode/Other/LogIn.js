587c3b4aa0424abb3995bc7b3143d5a1cb7bc147
SternGang <mkleeberg01@gmail.com>
2025-03-18 10:03:04 -0400

    updates to node

---
+++ .vscode/Other/LogIn.js
@@ -0,0 +1,15 @@
+let logInButton = document.getElementById("LogInButton");
+
+function checklog(){
+    let username = document.getElementById("UserName").value;
+    let PassWord = document.getElementById("PassWord").value;
+
+    console.log(username);
+    console.log(PassWord);
+
+    window.location.assign("MainPage.html");
+}
+
+
+
+logInButton.addEventListener("click",checklog);
