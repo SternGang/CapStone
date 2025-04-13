85d44750ebfe7f3bcd6005de713de5a4a55ece3d
KingSingh808 <115511968+KingSingh808@users.noreply.github.com>
2025-04-13 13:33:32 -0400

    updates

--- src/components/InputBone.jsx
+++ src/components/InputBone.jsx
@@ -3,18 +3,23 @@ const InputBone = () => {
     return (
         <div>
             <form>
+            <div className="column is-1"></div>
+            <div className="column is-1"></div>
+            <div className="column is-1"></div>
+            <div className="column is-1"></div>
+
                 <div className="columns">
                     <div className="field container column is-one-fifth">
                         <label>NAME</label>
-                        <input type="text" className="input" placeholder="Ex. John Doe" />
+                        <input type="text" className="input" placeholder="Ex. Raj Singh" />
                     </div>
                     <div className="field container column is-one-fifth">
                         <label>CLASS</label>
-                        <input type="text" className="input" placeholder="Ex. John Doe" />
+                        <input type="text" className="input" placeholder="Ex. 2025" />
                     </div>
                     <div className="field container  column is-one-fifth">
                         <label>COMPANY</label>
-                        <input type="text" className="input" placeholder="Ex. John Doe" />
+                        <input type="text" className="input" placeholder="Ex. Delta" />
                     </div>
                 </div>
 
@@ -23,37 +28,6 @@ const InputBone = () => {
             <div className="column is-1"></div>
             <div className="column is-1"></div>
 
-            <div className="">
-
-                <div className="dropdown is-hoverable">
-                    <div className="column is-4"></div>
-                    <div className="column is-4"></div>
-                    <div className="column is-4"></div>
-
-
-                    <div className="dropdown-trigger">
-                        <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
-                            <span>Dropdown button</span>
-                            <span className="icon is-small">
-                                <i className="fas fa-angle-down" aria-hidden="true"></i>
-                            </span>
-                        </button>
-                    </div>
-
-                    <div className="dropdown-menu" id="dropdown-menu" role="menu">
-
-                        <div className="dropdown-content">
-                            <a href="#" className="dropdown-item"> Dropdown item </a>
-                            <a className="dropdown-item"> Other dropdown item </a>
-                            <a href="#" className="dropdown-item is-active"> Active dropdown item </a>
-                            <a href="#" className="dropdown-item"> Other dropdown item </a>
-                            <hr className="dropdown-divider" />
-                            <a href="#" className="dropdown-item"> With a divider </a>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
             <div className="column is-4"></div>
             <div className="column is-4"></div>
 
